8df64f0566ee3a2b67e71ecbd11fb5a5a4b2acba
sanzhar <skantay03@gmail.com>
2024-07-13 04:15:39 +0500

    adding

--- internal/controller/user.go
+++ internal/controller/user.go
@@ -1,13 +1,19 @@
 package controller
 
 import (
+	"forum/internal/models"
 	"forum/internal/render"
 	"net/http"
 )
 
-type User struct {
-	r render.Render
+type userService interface {
+	Register(registration models.UserRegistration) error
 }
 
-func (u *User) Register(w http.ResponseWriter, r *http.Request) error {
+type User struct {
+	r render.Render
+	u userService
+}
+
+func (u *User) Register(w http.ResponseWriter, r *http.Request) {
 }
