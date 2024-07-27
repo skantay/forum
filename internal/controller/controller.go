57f3faaf48def547462216cfbe2e55edf639dd0a
sanzhar <skantay03@gmail.com>
2024-07-27 05:31:46 +0500

    reseting pakcages

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,17 +1,1 @@
 package controller
-
-import "net/http"
-
-type controller struct {
-	u user
-}
-
-func New() *http.ServeMux {
-	mux := http.NewServeMux()
-
-	ctrl := controller{}
-
-	mux.HandleFunc("sign_up", ctrl.u.register)
-
-	return mux
-}
--- internal/repository/user.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "forum/internal/models"
-
-type User struct{}
-
-func (u *User) SaveUser(user models.UserRegistration) error {
-
-}
